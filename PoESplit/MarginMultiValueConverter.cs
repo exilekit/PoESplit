bf09d7d15a06e5a75bd3e1c64185dcf4d1b0f586
Exile Kit <exilekit@gmail.com>
2023-08-05 15:41:46 -0400

    Stub the efficiency display using negative margins

---
+++ PoESplit/MarginMultiValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PoESplit
+{
+    class MarginMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Thickness(
+                System.Convert.ToDouble(values[0]),
+                System.Convert.ToDouble(values[1]),
+                System.Convert.ToDouble(values[2]),
+                System.Convert.ToDouble(values[3]));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PoESplit/SubtractHalfMultiValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PoESplit
+{
+    class SubtractHalfMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)values[0] - ((double)values[1] / 2.0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
